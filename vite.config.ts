a319f7c5cb51b77fd435deed6e1c86a23b3c662a
Zachintosh <zlw11063@gmail.com>
2022-09-02 13:56:51 -0600

    Clean out vite

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import VitePluginLinaria from 'vite-plugin-linaria'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), VitePluginLinaria()]
-})
