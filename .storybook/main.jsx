3daeb5b35f2c5236b00da4e75c27b1ac20b6f034
Zachintosh <zlw11063@gmail.com>
2022-09-02 13:49:20 -0600

    Remove postcss

--- .storybook/main.jsx
+++ .storybook/main.jsx
@@ -4,7 +4,6 @@ module.exports = {
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
-    '@storybook/addon-postcss',
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions"
